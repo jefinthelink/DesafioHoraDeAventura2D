e96e1c4db17ca655b067b21623206ec80ad1c822
jefinthelink <jefinzabotto@gmail.com>
2022-01-26 16:54:50 -0300

    arrumando evento onclick dos botoes

--- DesafioHoraDeAventura2D/Assets/Scripts/UISFX.cs
+++ DesafioHoraDeAventura2D/Assets/Scripts/UISFX.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UISFX : MonoBehaviour, IPointerEnterHandler
 {
+
 [SerializeField] private string stayAudioName = "Click";
+[SerializeField] private string clickAudioName = "Click2";
+private Button bt;
+
+private void Start() {
+    SetValues();
+}
+
+private void SetValues()
+{
+    bt = transform.GetComponent<Button>();
+    bt.onClick.AddListener(ClickSFX);
+}
     public void OnPointerEnter(PointerEventData eventData)
     {
        AudioHelper.instance.PlayAudio(stayAudioName);
+    }
+
+    void ClickSFX()
+    {
+        AudioHelper.instance.PlayAudio(clickAudioName);
     }
 
 }
