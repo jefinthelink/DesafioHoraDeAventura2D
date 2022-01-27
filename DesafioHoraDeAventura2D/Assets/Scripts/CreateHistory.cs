de9258e009a90762a1d6e59ef4119f083b7cb477
jefinthelink <jefinzabotto@gmail.com>
2022-01-27 19:02:26 -0300

    sistema de historia pronto

---
+++ DesafioHoraDeAventura2D/Assets/Scripts/CreateHistory.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Histories", menuName = "Histories")]
+public class CreateHistory : ScriptableObject
+{
+    public Sprite art;
+    public string description;
+}
---
+++ DesafioHoraDeAventura2D/Assets/Scripts/ShowInfo.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ShowInfo : MonoBehaviour
+{
+    [SerializeField] private ModesOfDhowInformation modes;
+    [SerializeField] private CreateTutorial[] createTutorial;
+    [SerializeField] private CreateHistory[] createHistories;
+    [SerializeField] private TMP_Text tutorialName;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private Image art;
+    [SerializeField] private GameObject content;
+    [SerializeField] private float timeToChangeHistory = 3.0f, timeToChangeHistoryAux;
+    private bool IsActive = false;
+
+    private int index = 0;
+private void Start() {
+    SetValues();
+}
+    private void Update() {
+        if(modes == ModesOfDhowInformation.history && IsActive)
+        {
+            timeToChangeHistoryAux -= Time.deltaTime;
+            if(timeToChangeHistoryAux <= 0.0f)
+            {
+                timeToChangeHistoryAux = timeToChangeHistory;
+                NextTutorial();
+            }
+        }
+    }
+private void ShowInformation(int index)
+    {
+        if(modes == ModesOfDhowInformation.tutorial)
+        {
+        tutorialName.text = createTutorial[index].tutorialName;
+        description.text = createTutorial[index].description;
+        art.sprite = createTutorial[index].art; 
+        }
+        else if(modes == ModesOfDhowInformation.history)
+        {
+        description.text = createHistories[index].description;
+        art.sprite = createHistories[index].art; 
+        }
+    }
+
+    private void SetValues()
+    {
+        timeToChangeHistoryAux = timeToChangeHistory;
+    }
+    public void NextTutorial()
+    {
+            index++;
+            if(modes == ModesOfDhowInformation.tutorial)
+            {
+                if (index < createTutorial.Length)
+                {
+                    ShowInformation(index);
+                }else
+                {
+                    HideTutorial();
+                }
+                
+            }
+            
+            else if(modes == ModesOfDhowInformation.history)
+            {
+                if (index < createHistories.Length)
+                {
+                    ShowInformation(index);
+                }
+                else
+                {
+                    SceneManager.LoadScene("GamePlay1");
+                }
+
+            }
+    }
+    public void ShowTutorial()
+    {
+        content.SetActive(true);
+        ShowInformation(index);
+        IsActive = true;
+    }
+    public void HideTutorial()
+    {
+        index = 0;
+        content.SetActive(false);
+    IsActive = false;
+    }
+
+}
+
+public enum ModesOfDhowInformation
+{
+tutorial,
+history
+}
